88ebfd1446d65405a21f9485ace49fd51e6e2554
Raman <24293550+ramank775@users.noreply.github.com>
2022-07-31 09:56:50 +0000

    update nats init script

--- deployment/scripts/init-nats.bash
+++ deployment/scripts/init-nats.bash
@@ -6,7 +6,7 @@ start_nats() {
 }
 
 create_stream () {
-  nats str add --subjects="$2" --storage=memory --replicas=1 --ack --retention=limits --discard=old --max-msgs=-1 --max-msgs-per-subject=-1 --max-bytes=-1 --max-age=-1 --max-msg-size=-1 --dupe-window=2m0s --allow-rollup --no-deny-delete --no-deny-purge $1
+  nats str add --subjects="$1.>" --storage=memory --replicas=1 --ack --retention=limits --discard=old --max-msgs=-1 --max-msgs-per-subject=-1 --max-bytes=-1 --max-age=-1 --max-msg-size=-1 --dupe-window=2m0s --allow-rollup --no-deny-delete --no-deny-purge $1
 }
 
 ENV_FILE=${1:-.env}
@@ -19,17 +19,9 @@ start_nats;
 
 sleep 2s;
 
-
-create_stream ${NATS_MESSAGE_DELIVERY_STREAM} "${TOPIC_USER_CONNECTION_STATE}.> ${TOPIC_SEND_MESSAGE}.>"
-sleep 1s;
-
-create_stream ${NATS_MESSAGE_ROUTER_STREAM} "${TOPIC_NEW_MESSAGE}.>"
-sleep 1s;
-
-create_stream ${NATS_GROUP_MESSAGE_ROUTER_STREAM} "${TOPIC_NEW_GROUP_MS}.>"
-sleep 1s;
-
-create_stream ${NATS_NOTIFICATION_STREAM} "${TOPIC_NEW_LOGIN}.> ${TOPIC_OFFLINE_MESSAGE}.>"
-sleep 1s;
+for var in "${!TOPIC_@}"; do
+    create_stream ${!var};
+    sleep 1s;
+done
 
 nats str ls
